57cde889689c16dd8837a60105b26d7b65834b19
jesskeepswimming <44094521+jesskeepswimming@users.noreply.github.com>
2021-05-08 20:33:39 -0400

    modularized code

--- server/db.js
+++ server/db.js
@@ -1,100 +1,98 @@
-const Pool = require("pg").Pool;
 
-const pool = new Pool({
-    user: "postgres",
-    password: "password2001",
-    host: "localhost",
-    port: 5432,
-    database: "plantDB"
-});
+class DB{
 
-async function newProfile(user_id, plant, for_sale, image, variety){
-    const newProfile = await pool.query(
-        `INSERT INTO plant_profiles (user_id, plant, for_sale, image, variety) 
-        VALUES($1, $2, $3, $4, $5) 
-        RETURNING plant_id`, 
-        [user_id, plant, for_sale, image, variety]
-    )
+    constructor(pool) {
+        this.pool = pool;
+    }
 
-    const plant_id = newProfile.rows[0].plant_id
+    async newProfile(user_id, plant, for_sale, image, variety){
+        const newProfile = await this.pool.query(
+            `INSERT INTO plant_profiles (user_id, plant, for_sale, image, variety) 
+            VALUES($1, $2, $3, $4, $5) 
+            RETURNING plant_id`, 
+            [user_id, plant, for_sale, image, variety]
+        )
 
-    return plant_id
+        const plant_id = newProfile.rows[0].plant_id
+
+        return plant_id
+    }
+
+    async newPost(plant_id, stage, caption, image){
+        const newPost = await this.pool.query(
+            `INSERT INTO plant_posts (plant_id, stage, caption, image) 
+            VALUES($1, $2, $3, $4) 
+            RETURNING post_id, date_posted`,
+            [plant_id, stage, caption, image]
+        )
+
+        return newPost.rows[0]
+    }
+
+    async updateProfile(image, date_posted, plant_id){
+        await this.pool.query(
+            `UPDATE plant_profiles 
+            SET image = $1, last_updated = $2 
+            WHERE plant_id = $3`, 
+            [image, date_posted, plant_id]
+        )
+
+        return 
+    }
+
+    async getAllProfiles(){
+        const allProfiles = await this.pool.query(`
+            SELECT * FROM plant_profiles 
+            ORDER BY last_updated DESC`
+        )
+
+        return allProfiles.rows
+    }
+
+    async searchProfiles(plant){
+        const allProfiles = await this.pool.query(`
+            SELECT * FROM plant_profiles 
+            WHERE plant = $1`,
+            [plant]
+        )
+
+        return allProfiles.rows
+    }
+
+    async getProfilePosts(plant_id){
+        const posts = await this.pool.query(`
+            SELECT * FROM plant_posts 
+            WHERE plant_id = $1 
+            ORDER BY date_posted DESC`, 
+            [plant_id]
+        )
+
+        return posts.rows
+    }
+
+    async deleteProfile(plant_id) {
+        await this.pool.query(`
+            DELETE FROM plant_profiles 
+            WHERE plant_id = $1`,
+            [plant_id]
+        );
+
+        await this.pool.query(`
+            DELETE FROM plant_posts 
+            WHERE plant_id = $1`, 
+            [plant_id]
+        );
+
+    }
+
 }
 
-async function newPost(plant_id, stage, caption, image){
-    const newPost = await pool.query(
-        `INSERT INTO plant_posts (plant_id, stage, caption, image) 
-        VALUES($1, $2, $3, $4) 
-        RETURNING post_id, date_posted`,
-        [plant_id, stage, caption, image]
-    )
-
-    return newPost.rows[0]
-}
-
-async function updateProfile(image, date_posted, plant_id){
-    await pool.query(
-        `UPDATE plant_profiles 
-        SET image = $1, last_updated = $2 
-        WHERE plant_id = $3`, 
-        [image, date_posted, plant_id]
-    )
-
-    return 
-}
-
-async function getAllProfiles(){
-    const allProfiles = await pool.query(`
-        SELECT * FROM plant_profiles 
-        ORDER BY last_updated DESC`
-    )
-
-    return allProfiles.rows
-}
-
-async function searchProfiles(plant){
-    const allProfiles = await pool.query(`
-        SELECT * FROM plant_profiles 
-        WHERE plant = $1`,
-        [plant]
-    )
-
-    return allProfiles.rows
-}
-
-async function getProfilePosts(plant_id){
-    const posts = await pool.query(`
-        SELECT * FROM plant_posts 
-        WHERE plant_id = $1 
-        ORDER BY date_posted DESC`, 
-        [plant_id]
-    )
-
-    return posts.rows
-}
-
-async function deleteProfile(plant_id) {
-    await pool.query(`
-        DELETE FROM plant_profiles 
-        WHERE plant_id = $1`,
-        [plant_id]
-    );
-
-    await pool.query(`
-        DELETE FROM plant_posts 
-        WHERE plant_id = $1`, 
-        [plant_id]
-    );
-
-    return
-}
-
-
-exports.pool = pool;
-exports.newProfile = newProfile;
-exports.newPost = newPost;
-exports.updateProfile = updateProfile;
-exports.getAllProfiles = getAllProfiles;
-exports.searchProfiles = searchProfiles;
-exports.getProfilePosts = getProfilePosts;
-exports.deleteProfile = deleteProfile;
+module.exports = DB;
+// exports.pool = pool;
+// exports.newProfile = newProfile;
+// exports.newPost = newPost;
+// exports.updateProfile = updateProfile;
+// exports.getAllProfiles = getAllProfiles;
+// exports.searchProfiles = searchProfiles;
+// exports.getProfilePosts = getProfilePosts;
+// exports.deleteProfile = deleteProfile;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 const makeApp = require('./app.js');
-const db = require('./db')
+const makeDatabase = require('./db.js');
 
+const Pool = require("pg").Pool;
 
+const pool = new Pool({
+    user: "postgres",
+    password: "password2001",
+    host: "localhost",
+    port: 5432,
+    database: "plantDB"
+});
+
+const db = new makeDatabase(pool)
 const app = makeApp(db)
 
 app.listen(5000, ()=> {
