0dbc4b33eeafdfbc8f73154e426140686661559d
jesskeepswimming <44094521+jesskeepswimming@users.noreply.github.com>
2021-05-31 04:04:38 -0400

    deployment

---
+++ server/seed.js
@@ -0,0 +1,19 @@
+const index = require('./index');
+const createDB = require('./db/createDB')
+
+
+async function seed() {
+    const pool = index.pool
+    const client = await pool.connect();
+
+    // build database tables
+    await client.query(createDB.users)
+    await client.query(createDB.pins)
+    await client.query(createDB.plants);
+
+    client.release()
+    await pool.end();
+
+}
+
+seed()
